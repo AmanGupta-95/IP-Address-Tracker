1da6cff52b9061369886edcd2135a46078e52ed3
Aman gupta <amangupta.95@outlook.com>
2021-08-26 20:48:28 +0530

    init

---
+++ src/components/ResultBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResultBox() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ResultBox
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SearchBox() {
+	return (
+		<div>
+			<input type="text" />
+			<button>
+				<svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+					<path fill="none" stroke="#000" strokeWidth="3" d="M2 1l6 6-6 6" />
+				</svg>
+			</button>
+		</div>
+	);
+}
+
+export default SearchBox;
